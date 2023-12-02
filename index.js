e4e3414970f7a82005377970c77f47ca299ba820
hanif365 <m.a.hanifkhan64@gmail.com>
2023-12-02 21:54:32 +0600

    fix: Resolve Vercel deployment issue(4)

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+const corsOptions = {
+  origin: "https://mailflow-server1.vercel.app",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 // Set up Multer for handling file uploads
